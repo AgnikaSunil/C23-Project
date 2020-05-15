0e7fbd715306b3d914bb006cbe0c0c1c93da6ff3
AgnikaSunil <64271448+AgnikaSunil@users.noreply.github.com>
2020-05-15 12:35:33 +0530

    Update Circle.js

--- Circle.js
+++ Circle.js
@@ -19,7 +19,7 @@ class Circle{
         strokeWeight(4);
         stroke("yellow");
         fill("orange");
-        ellipseMode(RADIUS)
+        ellipseMode(RADIUS);
         ellipse(posX,posY,this.width);
         pop();
     }
